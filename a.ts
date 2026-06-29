f876d3814ca0f35562c2d6a9c8dc0ae99c6d27a2
iaMJ <kiyomi@mja.moe>
2026-06-30 02:49:09 +0700

    stupid

--- a.ts
+++
@@ -1,9 +0,0 @@
-import { lyricsClient } from "./src/index.js";
-
-console.log("--- Testing client.getSynced(...) with ISRC ---");
-const result1 = await lyricsClient.getSynced("JPU902501968");
-console.log(result1.lyrics?.split("\n").slice(0, 3).join("\n"));
-
-console.log("\n--- Testing client.searchSynced(...) with Query ---");
-const result2 = await lyricsClient.searchSynced("Snow Drop Conton Candy");
-console.log(result2.lyrics?.split("\n").slice(0, 3).join("\n"));
